47b0d873501b3e5923ffaba81d196d618399ca09
Luis Mayta <luis.mayta@gmail.com>
2018-11-25 00:32:17 -0500

    Diseño copiado

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Rap2hpoutre\LaravelLogViewer\LaravelLogViewerServiceProvider::class,
 
         /*
          * Application Service Providers...
--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.index');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
