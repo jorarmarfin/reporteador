3244c7f02208d7324ebf204fb3556c518d4d39ea
Luis Mayta <luis.mayta@gmail.com>
2018-12-10 00:15:10 -0500

    Menu y pagina

---
+++ resources/views/layouts/menu/sider.blade.php
@@ -0,0 +1,56 @@
+<ul class="nav nav-main">
+    <li class="nav-active">
+        <a class="nav-link" href="layouts-default.html">
+            <i class="fas fa-home" aria-hidden="true"></i>
+            <span>Escritorio</span>
+        </a>                        
+    </li>
+    <li class="nav-parent">
+        <a class="nav-link" href="#">
+            <i class="fas fa-align-left" aria-hidden="true"></i>
+            <span>Menu Levels</span>
+        </a>
+        <ul class="nav nav-children">
+            <li>
+                <a>
+                    First Level
+                </a>
+            </li>
+            <li class="nav-parent">
+                <a class="nav-link" href="#">
+                    Second Level
+                </a>
+                <ul class="nav nav-children">
+                    <li>
+                        <a>
+                            Second Level Link #1
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                            Second Level Link #2
+                        </a>
+                    </li>
+                    <li class="nav-parent">
+                        <a class="nav-link" href="#">
+                            Third Level
+                        </a>
+                        <ul class="nav nav-children">
+                            <li>
+                                <a>
+                                    Third Level Link #1
+                                </a>
+                            </li>
+                            <li>
+                                <a>
+                                    Third Level Link #2
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </li>
+
+</ul>
